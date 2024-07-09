f7bc528138902f0daaddcbaa66d9c90591f9a210
JacinthLaval <47199523+JacinthLaval@users.noreply.github.com>
2024-07-09 17:09:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-pip install scikit-learn
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 
